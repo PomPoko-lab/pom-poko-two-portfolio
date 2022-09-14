65ee1f742c1dda4793926a20f8554729a3f72c16
ckennx <ckennx@gmail.com>
2022-09-14 17:38:36 -0500

    webpack configs

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import '../scss/styles.scss';
 import * as views from './views';
 import * as selectors from './selectors';
 
+// Render Home on root
+
 selectors.btnHome.addEventListener('click', views.renderHome);
 selectors.btnAbout.addEventListener('click', views.renderAbout);
 selectors.btnProjects.addEventListener('click', views.renderProjects);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const zopfli = require('@gfx/zopfli');
+const zlib = require('zlib');
 
 module.exports = {
   entry: './src/js/main.js',
@@ -26,7 +29,30 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: () => [require('autoprefixer')],
+                plugins: () => [
+                  require('autoprefixer'),
+                  new CompressionPlugin({
+                    compressionOptions: {
+                      numiterations: 15,
+                    },
+                    algorithm(input, compressionOptions, callback) {
+                      return zopfli.gzip(input, compressionOptions, callback);
+                    },
+                  }),
+                  new CompressionPlugin({
+                    filename: '[path][base].br',
+                    algorithm: 'brotliCompress',
+                    test: /\.(js|css|html|svg)$/,
+                    compressionOptions: {
+                      params: {
+                        [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                      },
+                    },
+                    threshold: 10240,
+                    minRatio: 0.8,
+                    deleteOriginalAssets: false,
+                  }),
+                ],
               },
             },
           },
